2e281ba2fe0c8336957416feb6ad6cf04cee1f80
testun <testun.db@gmail.com>
2022-12-18 21:47:49 +0500

    add DZ

---
+++ SeminarC#_006/DZTask_001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Number(int[] array)
+{
+    int num = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) { 
+            num++;
+        }
+    }
+    return num;
+}
+
+int[] CreateMassive(int count) {
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++) {
+        array[i] = GetNumber($"Введите число {i+1}: ");
+    }
+    return array;
+}
+
+int GetNumber(string text) {
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int n = GetNumber("Сколько чисел вы введете: ");
+
+int[] way = CreateMassive(n);
+Console.WriteLine($"[{string.Join(", " , way)}]");
+Console.WriteLine($"Чисел больше нуля: {Number(way)}");
---
+++ SeminarC#_006/DZTask_002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// y = k1 * x + b1, y = k2 * x + b2
+// k1 * x + b1 = k2 * x + b2
+// (k1 - k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    if ((k1 * x + b1) == y) Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");
+    else Console.WriteLine($"Не пересекаются");
+}
+
+int GetNumber(string text) {
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int b1 = GetNumber("Введите значение b1: ");
+int k1 = GetNumber("Введите значение k1: ");
+int b2 = GetNumber("Введите значение b2: ");
+int k2 = GetNumber("Введите значение k2: ");
+Intersection(b1, k1, b2, k2);
